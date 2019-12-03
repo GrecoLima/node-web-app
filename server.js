a3adfe5c7f1abc08a819b1f230181f716a395815
grecolima <greco_lima@hotmail.com>
2019-12-02 21:30:21 -0300

    adicionado rotas para arquivos html

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
+const router = express.Router();
 
 // Constants
 const PORT = 8080;
@@ -11,6 +13,13 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello world Teste Rebuild\n');
 });
+
+router.get('/index',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.use(express.static(__dirname + '/View'));
+app.use('/', router);
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
